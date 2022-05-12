39b9acf51e8825a152056d2f536f206442ff881d
Filip Bugarin <filip.bugarin@gmail.com>
2022-05-12 14:32:42 +0200

    final commit

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,185 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+
+  public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE = 'photos';
+  private platform: Platform;
+
+  constructor(platform: Platform,private http: HttpClient) {
+    this.platform = platform;
+  }
+
+  public async addNewToGallery() {
+
+    //Otvaranje kamere
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    //Iz Photo trebamo u UserPhoto pretvorit
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    //Spremanje u local storage
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
+
+    this.loadSaved();
+  }
+
+  removePhoto() {
+    this.photos = [];
+    Storage.clear();
+  }
+
+  public async loadSaved() {
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+
+    if (!this.platform.is('hybrid')) {
+      for (let photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }
+  }
+
+  public sendToBackend(data: string){
+    console.log(data);
+    let image = this.dataURLtoFile(data,'test');
+    const formData = new FormData();
+    formData.append('file',image);
+    this.http
+      .post('http://ip172-18-0-76-c9uetq09jotg00fqsaj0-8080.direct.labs.play-with-docker.com/api/image', formData, {responseType:'blob'})
+      .subscribe({
+        next: (response) => {
+          this.saveBlobPictureToList(response);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  private dataURLtoFile(dataurl, filename): File {
+    const arr = dataurl.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  }
+
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (this.platform.is('hybrid')) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
+  }
+
+  private async saveBase64Photo(base64Data: string){
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (this.platform.is('hybrid')) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      return {
+        filepath: fileName,
+        webviewPath: base64Data
+      };
+    }
+  }
+
+  private async saveBlobPictureToList(blob: Blob){
+    let data = await this.convertBlobToBase64(blob) as string;
+    let savedImageFIle = await this.saveBase64Photo(data);
+    this.photos.unshift(savedImageFIle);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
+  }
+
+  private async readAsBase64(photo: Photo) {
+    if (this.platform.is('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: photo.path,
+      });
+
+      return file.data;
+    } else {
+      const response = await fetch(photo.webPath);
+      const blob = await response.blob();
+
+      return (await this.convertBlobToBase64(blob)) as string;
+    }
+  }
+
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
